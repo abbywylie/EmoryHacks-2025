e656a74f370ce92451bbe0c0c091a3e443a6620e
pliaoxie-png <pliaoxie@gmail.com>
2025-11-14 21:17:18 -0500

    gle log in final (front end)

--- login.js
+++ login.js
@@ -1,28 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // function onSignIn(googleUser) {
-    //     var profile = googleUser.getBasicProfile();
-    //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    //     console.log('Name: ' + profile.getName());
-    //     console.log('Image URL: ' + profile.getImageUrl());
-    //     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    // }
-    function handleCredentialResponse(response) {
-      // Google sends back a JWT token
-      const jwt = response.credential;
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
 
-      console.log("JWT ID Token:", jwt);
+function handleCredentialResponse(response) {
+    // Google sends back a JWT token
+    const jwt = response.credential;
 
-      // Decode it (optional: only for front-end display)
-      const payload = JSON.parse(atob(jwt.split('.')[1]));
+    console.log("JWT ID Token:", jwt);
 
-      console.log("User Info:", payload);
+    // Decode it (optional: only for front-end display)
+    const payload = JSON.parse(atob(jwt.split('.')[1]));
 
-      // Example: show user info
-      document.getElementById("userInfo").innerHTML = `
-          <p><strong>Name:</strong> ${payload.name}</p>
-          <p><strong>Email:</strong> ${payload.email}</p>
-          <img src="${payload.picture}" />
-      `;
-  }
-    
-})
+    console.log("User Info:", payload, payload.email);
+
+    // Example: show user info
+    document.getElementById("userInfo").innerHTML = `
+        <p class="right-aligned">Welcome, <strong>${payload.name}</strong></p>
+        <img src="${payload.picture}" class="right-aligned"/>
+    `;
+
+    document.getElementById("signIn").remove();
+}
